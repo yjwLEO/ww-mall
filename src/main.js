7253f5c074ade79853290726438a2d1027a89050
叶继伟 <a19980510>
2020-02-28 02:46:04 +0800

    解决移动端300毫秒适配

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+import "babel-polyfill";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import fastclick from "fastclick";
 
-import "./assets/scss/index.scss";
+import "assets/scss/index.scss";
+
+//消除移动端300ms的延迟
+fastclick.attach(document.body);
 
 Vue.config.productionTip = false;
 
