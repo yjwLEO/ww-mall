5ccc0983acb345c3288a1cd7a079a848926a2872
叶继伟 <a19980510>
2020-02-28 03:17:22 +0800

    tabbar组件完成

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,19 @@
     <div class="g-view-container">
       <router-view></router-view>
     </div>
-    <div class="g-footer-container"></div>
+    <div class="g-footer-container">
+      <c-tab-bar></c-tab-bar>
+    </div>
   </div>
 </template>
 
 <script>
+import CTabBar from "components/tabbar";
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    CTabBar
+  }
 };
 </script>
 <style lang="scss"></style>
