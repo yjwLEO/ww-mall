209032ef5a91109d5dd89fe7003df7637616e940
叶继伟 <a19980510>
2020-02-28 02:46:29 +0800

    配置别名完成

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,9 @@
   <div id="app"></div>
 </template>
 
+<script>
+export default {
+  name: "App"
+};
+</script>
 <style lang="scss"></style>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require("path"); //引入path模块
+function resolve(dir) {
+  return path.join(__dirname, dir); //path.join(__dirname)设置绝对路径
+}
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      extensions: [".js", "vue", ".json"], //这些文件的后缀名可以不写
+      alias: {
+        base: resolve("src/base"),
+        api: resolve("src/api"),
+        components: resolve("src/components"),
+        assets: resolve("src/assets"),
+        pages: resolve("src/pages")
+      }
+    }
+  }
+};
