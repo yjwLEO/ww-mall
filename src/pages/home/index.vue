d3ce2f33bc7fdbb64fd4d6d230547f31d4c4cd31
叶继伟 <a19980510>
2020-02-28 12:39:56 +0800

    首页结构和样式完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    home
+    <div class="g-header-container"></div>
+    <div></div>
+    <div class="g-backtop-container">asdasd</div>
     <router-view></router-view>
   </div>
 </template>
@@ -11,4 +13,9 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "~assets/scss/mixins";
+.home {
+ overflow:hidden;width:100%;height:100%;background-color: $bgc-theme;
+}
+</style>
